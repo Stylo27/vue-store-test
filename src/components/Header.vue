3a3c89c287a69bbf14de993e3d0807f638173d50
Anton Dymov <anton.dymov@chisw.com>
2019-05-28 13:17:41 +0300

    Install vuetify

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>C</span>
+      <span class="font-weight-light">atalog</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
